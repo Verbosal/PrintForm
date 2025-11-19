d16455c50f4738789b843bd68279ea713028681b
Pozdro70 <mateusz_wit@wp.pl>
2025-11-19 20:52:52 +0100

    submits list

---
+++ list.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'database.php';
+$printsQuery = $db->query('SELECT * FROM druki');
+$prints=$printsQuery->fetchAll(PDO::FETCH_ASSOC);
+
+echo("<table>");
+
+echo("<tr><th>Link</th><th>Imię</th><th>Notatka</th><th>Nazwa pliku</th><th>Kolory</th></tr>");
+
+foreach($prints as $print){
+    echo("<tr>");
+    $wypisanoID=false;
+    foreach($print as $pdata){
+        if($wypisanoID){echo("<td>$pdata</td>");}
+        $wypisanoID=true;
+    }
+    echo("</tr>");
+}
+echo("</table>");
+?>
+<html>
+    <style>
+        table, th, td {
+        border: 1px solid white;
+        border-collapse: collapse;
+        }
+        th, td {
+        background-color: #c5e6e6ff;
+        }
+    </style>
+</html>
