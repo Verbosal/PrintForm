71a8d07a6ba00e493d2d044c43db745f9e3e7c4f
Pozdro70 <mateusz_wit@wp.pl>
2025-11-20 15:59:57 +0100

    file upload logic

--- addData.php
+++ addData.php
@@ -2,11 +2,49 @@
 if(isset($_POST['sender'])){
     require_once 'database.php';
 
+    $target_dir = "pliki/";
+
+    $pfinfo = pathinfo($_FILES["fname"]["name"]);
+    $newFNAME = $pfinfo['filename'] . rand() . '.' . $pfinfo['extension'];
+
+    $target_file = $target_dir . basename($newFNAME);
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $uploadOk = 1;
+
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+        exit();
+    }
+
+    if ($_FILES["fname"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+        exit();
+    }
+
+    if($imageFileType != "stl" && $imageFileType != "step" && $imageFileType != "3mf"
+    && $imageFileType != "obj" ) {
+        echo "Typ pliku sie nie zgadza";
+        $uploadOk = 0;
+        exit();
+    }
+
+    if ($uploadOk == 0) {
+        echo "Ełror!";
+    } else {
+        if (move_uploaded_file($_FILES["fname"]["tmp_name"], $target_file)) {
+            echo "Plik ". htmlspecialchars( basename( $_FILES["fname"]["name"])). " został zaplołdowany do druku";
+        } else {
+            echo "jakiś błąd przy uploadzie spróbuj jeszcze raz";
+        }
+    }
+
     $query = $db->prepare('INSERT INTO druki VALUES (NULL, :link, :sender, :note, :fname, :colors)');
     $query->bindValue(':link',$_POST['link'],PDO::PARAM_STR);
     $query->bindValue(':sender',$_POST['sender'],PDO::PARAM_STR);
     $query->bindValue(':note',$_POST['note'],PDO::PARAM_STR);
-    $query->bindValue(':fname',$_POST['fname'],PDO::PARAM_STR);
+    $query->bindValue(':fname',$newFNAME,PDO::PARAM_STR);
     //$query->bindValue(':colors',$_POST['???'],PDO::PARAM_STR); czekam jak zrobisz kolorki
     $query->bindValue(':colors',"none",PDO::PARAM_STR);
     $query->execute();
